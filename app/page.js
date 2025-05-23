fb319d99d6bc50a3e328cb801c41d052f9c03441
Junheng Zheng <jz7259@g.rit.edu>
2025-05-22 23:15:05 -0400

    mobile view progress

--- app/page.js
+++ app/page.js
@@ -16,17 +16,8 @@ const Page = () => {
       <FloatingHeader />
       <div className="w-full h-fit flex flex-col items-center justify-center bg-white ">
         <div className="p-[24px] flex-1 w-full">
-          <div className="rainbow-radial w-full flex flex-col p-[72px] pb-[0px] items-start gap-[48px] h-full rounded-[24px]">
+          <div className="rainbow-radial w-full flex flex-col sm:p-[72px] p-[24px] pb-[0px] items-center gap-[16px] sm:gap-[48px] h-full rounded-[24px]">
             <div className="flex w-full flex-col h-full justify-center items-center gap-[16px]">
-              {/* <motion.div
-                initial={{ opacity: 0, y: 50 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.3 }}
-                className="flex items-center gap-[16px] p-[8px] w-[180px] text-white rounded-lg rainbow-radial justify-center"
-              >
-                <p className="text-[14px]">AI-Autocomplete</p>
-                <i className="text-[14px] fa-solid fa-pencil"></i>
-              </motion.div> */}
               <motion.div
                 initial={{ opacity: 0, y: 50 }}
                 animate={{ opacity: 1, y: 0 }}
@@ -39,7 +30,7 @@ const Page = () => {
                 initial={{ opacity: 0, y: 50 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.3, delay: 0.1 }}
-                className=" font-semibold text-[64px] text-black !leading-[1.2] tracking-[-1.2px]"
+                className=" font-semibold sm:text-[64px] text-center text-[32px] text-black !leading-[1.2] tracking-[-1.2px]"
               >
                 WRITING + AI FOR STUDENTS
               </motion.h1>
@@ -47,7 +38,7 @@ const Page = () => {
                 initial={{ opacity: 0, y: 50 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.3, delay: 0.2 }}
-                className="text-[21px]  !leading-[1.2] text-black"
+                className="sm:text-[21px]  text-center !leading-[1.2] text-black"
               >
                 Autocomplete. Real-time Collaboration. Undetectable - Say
                 goodbye to docs.
@@ -57,6 +48,7 @@ const Page = () => {
                   initial={{ opacity: 0, y: 50 }}
                   animate={{ opacity: 1, y: 0 }}
                   transition={{ duration: 0.3, delay: 0.3 }}
+                  className="hidden sm:flex"
                 >
                   <Button
                     size="large"
@@ -81,7 +73,7 @@ const Page = () => {
               <img
                 src="/assets/example.png"
                 alt="example"
-                className="object-cover w-[70%] h-full object-top rounded-t-[12px]"
+                className="object-cover sm:w-[70%] h-full object-top rounded-t-[12px]"
               />
             </div>
           </div>
